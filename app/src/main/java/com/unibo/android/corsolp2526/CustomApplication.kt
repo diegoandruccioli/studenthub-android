b286cefec4411c7a931f071925633875dc264160
Diego Andruccioli <diego.andruccioli@studio.unibo.it>
2026-04-27 10:01:53 +0200

    feat(app): wire CustomApplication as RepositoryProvider for manual dependency injection

--- app/src/main/java/com/unibo/android/corsolp2526/CustomApplication.kt
+++ app/src/main/java/com/unibo/android/corsolp2526/CustomApplication.kt
@@ -2,11 +2,20 @@ package com.unibo.android.corsolp2526
 
 import android.app.Application
 import com.unibo.android.data.di.RepositoryProviderImpl
+import com.unibo.android.domain.di.RepositoryProvider
 import com.unibo.android.domain.di.UseCasesProvider
+import com.unibo.android.domain.repository.EsameRepository
 
-class CustomApplication : Application() {
+class CustomApplication : Application(), RepositoryProvider {
+
+    private lateinit var repositoryProviderImpl: RepositoryProviderImpl
+
     override fun onCreate() {
         super.onCreate()
-        UseCasesProvider.setup(RepositoryProviderImpl())
+        repositoryProviderImpl = RepositoryProviderImpl(this)
+        UseCasesProvider.setup(repositoryProviderImpl)
     }
+
+    override fun getEsameRepository(): EsameRepository =
+        repositoryProviderImpl.getEsameRepository()
 }
