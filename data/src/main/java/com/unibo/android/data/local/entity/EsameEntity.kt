d9a09c2bad42cba2474b72b44f250e52cd32a69d
Giovanni Morelli <giovanni.morelli8@studio.unibo.it>
2026-05-15 17:44:29 +0200

    refactor(data): change dataEsame type to LocalDate in EsameEntity

--- data/src/main/java/com/unibo/android/data/local/entity/EsameEntity.kt
+++ data/src/main/java/com/unibo/android/data/local/entity/EsameEntity.kt
@@ -4,6 +4,8 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+import java.time.LocalDate
+
 @Entity(tableName = "esami")
 data class EsameEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
@@ -11,7 +13,7 @@ data class EsameEntity(
     @ColumnInfo(name = "voto") val voto: Int,
     @ColumnInfo(name = "lode") val lode: Boolean,
     @ColumnInfo(name = "cfu") val cfu: Int,
-    @ColumnInfo(name = "data_esame") val dataEsame: String,
+    @ColumnInfo(name = "data_esame") val dataEsame: LocalDate,
     @ColumnInfo(name = "remote_id") val remoteId: Int? = null,
     @ColumnInfo(name = "pending_sync") val pendingSync: Boolean = true
 )
