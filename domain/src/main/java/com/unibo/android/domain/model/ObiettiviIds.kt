0c076b272948a93a73f7e39842733952f1b88243
Giovanni Morelli <giovanni.morelli8@studio.unibo.it>
2026-05-15 17:27:19 +0200

    feat(domain): add ObiettiviIds constants to avoid magic numbers

---
+++ domain/src/main/java/com/unibo/android/domain/model/ObiettiviIds.kt
@@ -0,0 +1,8 @@
+package com.unibo.android.domain.model
+
+object ObiettiviIds {
+    const val PRIMO_PASSO = 1
+    const val SECCHIONE = 2
+    const val MARATONETA = 3
+    const val GIRO_DI_BOA = 4
+}
