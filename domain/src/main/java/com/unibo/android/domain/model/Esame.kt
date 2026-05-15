7e3478ae304d9dfe76aa4a5946b7c97682568b39
Giovanni Morelli <giovanni.morelli8@studio.unibo.it>
2026-05-15 17:42:38 +0200

    refactor(domain): change dataEsame type to LocalDate in Esame model

--- domain/src/main/java/com/unibo/android/domain/model/Esame.kt
+++ domain/src/main/java/com/unibo/android/domain/model/Esame.kt
@@ -1,10 +1,12 @@
 package com.unibo.android.domain.model
 
+import java.time.LocalDate
+
 data class Esame(
     val id: Int = 0,
     val nome: String,
     val voto: Int,
     val lode: Boolean,
     val cfu: Int,
-    val dataEsame: String
+    val dataEsame: LocalDate
 )
